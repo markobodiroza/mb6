3ce22585bf525bfb9b6fe2e3a9b0c624d713bd90
Marko Bodiroža <omaraston@gmail.com>
2023-08-17 07:50:43 +0000

    Mb6

---
+++ src/pages/next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://markobodiroza.com/',
+      siteName: 'Marko Bodiroza',
+    },
+    twitter: {
+      handle: '@markobodiroza',
+      site: '@markobodiroza',
+      cardType: 'summary_large_image',
+    },
+  };
