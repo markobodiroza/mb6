6cdec51c15c3997c80560bd4f0811c45af4d0850
Marko Bodiroža <omaraston@gmail.com>
2023-08-17 09:26:08 +0000

    AG

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,14 +1,14 @@
 import { Container } from '@/components/Container'
 import { Prose } from './Prose'
-export function PageLayout({ meta, children }) {
+export function PageLayout({ title, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-slate-800 dark:text-slate-100 sm:text-5xl">
-          {meta.title}
+         {title}
         </h1>
         <p className="mt-6 text-lg text-slate-700 dark:text-slate-400">
-          {meta.intro}
+          {intro}
         </p>
       </header>
       <Prose className="mt-8">{children}</Prose>
