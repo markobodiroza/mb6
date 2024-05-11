c3ef1f9863e984df619a3dd58ee18dfe16c7cc6a
“Marko” <omaraston@gmail.com>
2024-05-11 08:09:44 +0200

    Page Speed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,7 +7,7 @@ import { DefaultSeo } from 'next-seo';
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { MDXProvider } from '@mdx-js/react'
 import  Pre  from '@/components/Pre'
@@ -49,6 +49,7 @@ export default function App({ Component, pageProps, router }) {
         <Footer />
       </div>
     </MDXProvider>
+    <SpeedInsights />
     </>
   )
 }
