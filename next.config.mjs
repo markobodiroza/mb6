58eb316fbace940dc4f8c66c107b86bc05c9c2bb
Marko Bodiroza <omaraston@gmail.com>
2025-03-29 08:17:26 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,13 @@ import remarkGfm from 'remark-gfm'
 import rehypePrismPlus from 'rehype-prism-plus'
 import remarkCodeTitles from './src/lib/remark-code-title.mjs'
 import rehypePresetMinify from 'rehype-preset-minify'
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
-  },
 }
 
 const withMDX = nextMDX({
