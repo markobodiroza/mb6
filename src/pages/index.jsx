05c029857b83911e968dd6e197422b991095b911
Marko Bodiroza <omaraston@gmail.com>
2025-03-30 08:01:24 +0200

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,29 +18,6 @@ import  siteMeta, {resume} from '@/data/siteMeta'
 import { NextSeo } from 'next-seo';
 
 
-function MailIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-neutral-100 stroke-neutral-400 dark:fill-neutral-100/10 dark:stroke-neutral-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-neutral-400 dark:stroke-neutral-500"
-      />
-    </svg>
-  )
-}
-
 function BriefcaseIcon(props) {
   return (
     <svg
@@ -97,35 +74,6 @@ function SocialLink({ icon: Icon, ...props }) {
     <Link className="p-1 -m-1 group" {...props}>
       <Icon className="w-6 h-6 transition fill-neutral-500 group-hover:fill-neutral-700 dark:fill-neutral-400 dark:group-hover:fill-neutral-300" />
     </Link>
-  )
-}
-
-function Newsletter() {
-  return (
-    <form
-      action="/thank-you"
-      className="p-6 border rounded-2xl border-neutral-100 dark:border-neutral-700/40"
-    >
-      <h2 className="flex text-sm font-semibold text-neutral-900 dark:text-neutral-100">
-        <MailIcon className="flex-none w-6 h-6" />
-        <span className="ml-3">Don't miss out</span>
-      </h2>
-      <p className="mt-2 text-sm text-neutral-700 dark:text-neutral-400">
-        Get notified when I publish something new, and unsubscribe at any time.
-      </p>
-      <div className="flex mt-6">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-neutral-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-neutral-800/5 placeholder:text-neutral-400 focus:border-emerald-500 focus:outline-none focus:ring-4 focus:ring-emerald-500/10 dark:border-neutral-700 dark:bg-neutral-700/[0.15] dark:text-neutral-200 dark:placeholder:text-neutral-500 dark:focus:border-emerald-400 dark:focus:ring-emerald-400/10 sm:text-sm"
-        />
-        <Button type="submit" className="flex-none ml-4">
-          Join
-        </Button>
-      </div>
-    </form>
   )
 }
 
