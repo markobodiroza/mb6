129ffaa69ad30b9fcab61fb59c34388c698f4088
Marko Bodiroza <omaraston@gmail.com>
2025-03-30 07:45:13 +0200

    Update PageLayout.jsx

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -4,7 +4,7 @@ export function PageLayout({ title, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
-        <h1 className="text-5xl font-bold tracking-tight text-neutral-800 dark:text-neutral-100 sm:text-5xl">
+        <h1 className="text-4xl font-bold tracking-tight text-neutral-800 dark:text-neutral-100 sm:text-5xl">
          {title}
         </h1>
         <p className="mt-6 text-lg text-neutral-700 dark:text-neutral-400">
